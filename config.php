ce2badf816f9502b44a8c7ef1ef031c18c9a8f84
unkaer <ababwbq@outlook.com>
2020-10-02 17:30:14 +0800

    修复莫名其妙的访问bug

--- config.php
+++ config.php
@@ -6,13 +6,13 @@ $fdm = true;  // 默认打开true，关闭 false
 // get 第三方视频链接
 $fget_html = true;
 // // 完整版
-// $dm1=array('http://flvweb.com','http://www.nicotv.me','http://www.zzzfun.com','https://www.agefans.tv');
+// $dm1=array('https://flvweb.com','http://www.nicotv.me','http://www.zzzfun.com','https://www.agefans.tv');
 // $dm2=array('/index.php/vod/search.html?wd=xxx','/video/search/xxx.html','/vod-search.html?wd=xxx.html','/search?query=xxx&page=1');
 // $dm3=array('flv','妮可动漫','zzzfun动漫视频网','AGE动漫');
 // $rule1=array("/href=\"(.*?)\" title=\"(.*?)\" data-original=/","/<a href=\"(.*?)\" title=\"(.*?)\">/","/<a href=\"(\/vod-detail.*?)\">(.*?)<\/a>/","/<a href=\"(.*?)\" class=\"cell_imform_name\">(.*?)<\/a>/");
 // $rule2=array('/<span class="pic-text text-right">([\S\s]*?)<\/span>/','/<span class="continu">([\S\s]*?)<\/span>/','/<span class="color">([\S\s]*?)<\/span>/','/<span class="newname">(.*?)<\/span>/');
 // 精简版
-$dm1=array('http://flvweb.com');
+$dm1=array('https://flvweb.com');
 $dm2=array('/index.php/vod/search.html?wd=xxx');
 $dm3=array('flv');
 $rule1=array("/href=\"(.*?)\" title=\"(.*?)\" data-original=/");
