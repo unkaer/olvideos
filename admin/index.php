8ea08d13335f1dc5ae0f0583c070ba1087f7463e
unkaer <ababwbq@outlook.com>
2020-10-02 19:59:00 +0800

    更新bug

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,46 @@
+-<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <title>后台管理</title>
+    </head>
+    <body>
+        <div><a href="..">回到首页</a></div>
+        <div>
+<?php
+//  防止全局变量造成安全隐患
+$admin = false;
+//  启动会话，这步必不可少
+session_start();
+//  判断是否登陆
+if (isset($_SESSION["admin"]) && $_SESSION["admin"] === true) {
+    echo "<p>您已经成功登陆</p>";
 
-<form action="./paqu.php" method='POST'>
-    <p>get网页测试：</p>
-    <p>url<input type="text" name="url" value="">
-    <input type="submit" value="爬取"></p>
-</form>
+    // 更新系统
+    echo '<div><a href="./down.php?id=1">更新系统</a></div>';
+    echo '<div><a href="./down.php?id=4">清除首页缓存</a></div>';
+    echo '<div><a href="./down.php?id=5">搜索数据管理</a></div>';
+    echo '<div><a href="./down.php?id=8">系统设置</a></div>';
 
-<form action="./api.php" method='POST'>
-    <p>post网页测试：</p>
-    <p>url<input type="text" name="url" value=""></p>
-    <p>名称<input type="text" name="na1" value=""></p>
-    <p>内容<input type="text" name="na2" value="">
-    <input type="submit" value="搜索"></p>
-</form>
-
-<form action="./dm.php" method='POST'>
-    <p>post网页：<input type="text" name="wd" value="">
-    <input type="submit" value="搜索"></p>
-</form>
-        
+    echo "<h3>用户访问记录：</h3>";
+    $file="../data/.log";
+    if(file_exists($file)){
+        $handle=fopen($file,'r');
+        $log=fread($handle,filesize($file));
+        print_r($log);
+    }
+} else {
+    //  验证失败，将 $_SESSION["admin"] 置为 false
+    $_SESSION["admin"] = false;
+    echo("您无权访问，请登录");
+    print_r('
+    <form action="./login.php" method="POST">
+    <p>用户：<input id="ipt" type="text" name="username" autofocus value="">
+    <p>密码：<input id="ipt" type="text" name="password" autofocus value="">
+        <input type="submit" value="登录"></p>
+    </form>');
+}
+?>
+        </div>
+    </body>
+</html>
