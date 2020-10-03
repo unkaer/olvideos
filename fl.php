391f509d41a7be7ae65fba9ce28521b6fa72e7bb
unkaer <ababwbq@outlook.com>
2020-10-03 22:04:54 +0800

    添加多页功能

--- fl.php
+++ fl.php
@@ -6,6 +6,12 @@ if(array_key_exists("fw", $_POST)|array_key_exists("fw", $_GET)){
     if(array_key_exists("cid", $_POST)|array_key_exists("cid", $_GET)){
         if(isset($_POST["cid"])){$cid = $_POST["cid"];}else{$cid = $_GET["cid"];}
     }
+    if(array_key_exists("gx", $_POST)|array_key_exists("gx", $_GET)){
+        if(isset($_POST["gx"])){$gx = $_POST["gx"];}else{$gx = $_GET["gx"];}
+    }
+    if(array_key_exists("pg", $_POST)|array_key_exists("pg", $_GET)){
+        if(isset($_POST["pg"])){$pg = $_POST["pg"];}else{$pg = $_GET["pg"];}
+    }else{$pg=1;}
 }
 else{
     // 未选择服务器
@@ -40,6 +46,7 @@ else{
         <form action="./fl.php" method='POST'>
             <input type="hidden" name="fw" autofocus value="<?php echo $fw;?>">
             <input type="hidden" name="cid" autofocus value="<?php echo $cid;?>">
+            <input type="hidden" name="pg" autofocus value="<?php echo $pg;?>">
             <input type="hidden" name="gx" value="1">
             <p>如果没有搜索结果，请减少关键词
             <input type="submit" value="更新本页面"></p>
@@ -83,8 +90,16 @@ function getcid($api,$api1){
 
 //API 获取视频id geturl视频信息
 function getlist($api,$t,$f){
-    $data = file_get_contents($api."?ac=list&t=".$t);
+    global $array,$pg;
+    $data = file_get_contents($api."?ac=list&t=".$t."&pg=".$pg);
+    // print_r($api."?ac=list&t=".$t."&pg=".$pg);
     $xml = simplexml_load_string($data);
+    foreach($xml->list->attributes() as $a => $b){
+        // echo $a."=".$b."<br>";
+        $array['list'][$a]=(string)$b;
+    }
+    if($f){build1();}
+    // print_r($array['list']);
     foreach($xml->list->video as $video){
         $id=(string)$video->id;
         geturl($id,$api,"",$f);
@@ -99,6 +114,7 @@ function geturl($id,$api,$api1,$f){
         $type=(string)$video->type; //类型
         $year=(string)$video->year; //上映时间
         $des=(string)$video->des; //简介
+        // $pic=str_replace("https","http",$pic);    // 解决部分封面
         $pic=(string)$video->pic; //封面
         $url=(string)$video->dl->dd;   //播放地址
         preg_match_all("/https?:\/\/[^#]*\/index.m3u8/",$url,$playurl);
@@ -123,14 +139,14 @@ function geturl($id,$api,$api1,$f){
 }
 
 function build($f){
-    global $array,$n,$file,$name,$fw,$cid;
+    global $array,$n,$file,$name,$fw,$cid,$pg;
     $luanma=array("<",">","rgb","(17, 17, 17)","&nbsp","span","style","="," ","color",":","13px","font-family","Helvetica",",","Arial","sans-serif",";","font-size","\"","/","br"); //部分简介乱码
     for($i=0;$i<sizeof($luanma);$i++){
         $array[$n]["des"]=str_replace($luanma[$i],"",$array[$n]["des"]);
     }
     if(isset($array[$n]["title"])){
         if( (!strstr($array[$n]["type"],"伦理")) && (!strstr($array[$n]["type"],"福利")) ){   // 屏蔽部分搜索结果
-            print_r('<div id="playul"><p>'.$array[$n]["zy"].'</p><div><a id="cover" href="./play.php?wd=api'.$fw.'id'.$cid.'&f=1&id='.$n.'" target="_blank" title="'.$array[$n]["des"].'" style="background-image: url('.$array[$n]["cover"].')">');  // 封面
+            print_r('<div id="playul"><p>'.$array[$n]["zy"].'</p><div><a id="cover" href="./play.php?wd=api'.$fw.'id'.$cid.'pg'.$pg.'&f=1&id='.$n.'" target="_blank" title="'.$array[$n]["des"].'" style="background-image: url('.$array[$n]["cover"].')">');  // 封面
             print_r("<span class=\"type\" >".$array[$n]["type"]."</span>");
             print_r("<span class=\"year\" >".$array[$n]["year"]."</span></a>");
             // if($array[$n]["download"][0]!="暂无"){
@@ -150,12 +166,24 @@ function build($f){
     }
 }
 
+function build1(){
+    global $array,$fw,$cid;
+    // print_r($array['list']);
+    $i = (int)$array['list']['page'] +1;
+    $j = (int)$array['list']['page'] -1;
+    if($array['list']['page']=="1"){ //首页
+        echo '<div><p><a href="./fl.php?fw='.$fw.'&cid='.$cid.'&pg='.$i.'">下一页 </a>视频总数'.$array['list']['recordcount'].'总页数'.$array['list']['page'].'/'.$array['list']['pagesize'].'</p></div>';
+    }elseif($array['list']['page']==$array['list']['pagesize']){ // 尾页
+        echo '<div><p><a href="./fl.php?fw='.$fw.'&cid='.$cid.'&pg='.$j.'">上一页 </a>视频总数'.$array['list']['recordcount'].'总页数'.$array['list']['page'].'/'.$array['list']['pagesize'].'</p></div>';
+    }else{
+        echo '<div><p><a href="./fl.php?fw='.$fw.'&cid='.$cid.'&pg='.$j.'">上一页 </a><a href="./fl.php?fw='.$fw.'&cid='.$cid.'&pg='.$i.'">下一页 </a>视频总数'.$array['list']['recordcount'].'总页数'.$array['list']['page'].'/'.$array['list']['pagesize'].'</p></div>';
+    }
+}
+
 function getarray($f){
     global $api,$api1,$url,$url1,$name;
     // 只最大 API   // 节约服务器
     getname($api[1],"路线-1",$f);
-
-    
     // 只ok 爬取   // 节约服务器
     $html = file_get_contents($url[0]."?m=vod-search&wd=".$name);   // 爬虫方式
     preg_match_all("/\?m=vod-detail-id-.+.html/",$html,$detail);
@@ -163,6 +191,8 @@ function getarray($f){
         playdetail($url[0].$x_value,"路线-2",$f);
     }
 }
+
+$list=array();
 
 if(!isset($api[$fw])){
     echo "服务器不存在";
@@ -172,7 +202,7 @@ if(!isset($api[$fw])){
 }
 
 if(isset($cid)){   // 某分类的视频  2
-    $file = "./data/api".$fw."id".$cid.".dp";
+    $file = "./data/api".$fw."id".$cid."pg".$pg.".dp";
     //  判断是否更新
     if(file_exists($file)){
         $handle=fopen($file,'r');// 存在 读取内容 只建立网页  只API 只爬取 
@@ -183,6 +213,7 @@ if(isset($cid)){   // 某分类的视频  2
                 file_put_contents($file,serialize($array));//写入缓存 
             }
         }
+        build1(); // 下页
         for($n=0;$n<sizeof($array);$n++){
             build(false);  // 只建立网页，不更新内容
         }
