c10938f8f5d542b4ae92d1f8b94550a7e60b3a38
ababwbq <ababwbq@outlook.com>
2021-02-16 18:39:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include './config.php';
 include './src/function.php';
-
+ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)');
 print_r($head);  //网页开头
 
 echo '<body>';
